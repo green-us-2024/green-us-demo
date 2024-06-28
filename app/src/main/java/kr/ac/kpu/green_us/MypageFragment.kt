18de4c0376f60f7a687106e50a8dd9e56c6c0fdf
sejinzx <132969962+sejinzx@users.noreply.github.com>
2024-06-28 23:20:45 +0900

    프로필 관리(이미지 크롭, 갤러리 차후 수정 예정)

--- app/src/main/java/kr/ac/kpu/green_us/MypageFragment.kt
+++ app/src/main/java/kr/ac/kpu/green_us/MypageFragment.kt
@@ -8,17 +8,26 @@ import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import kr.ac.kpu.green_us.databinding.FragmentHomeBinding
+import kr.ac.kpu.green_us.databinding.FragmentJoin1Binding
 import kr.ac.kpu.green_us.databinding.FragmentMypageBinding
 
 class MypageFragment : Fragment() {
+
+    lateinit var binding: FragmentMypageBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentMypageBinding.inflate(inflater,container,false)
+        binding.profileSetting.setOnClickListener {
+            val intent = Intent(getActivity(), MyProfileActivity::class.java)
+            startActivity(intent)
+        }
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mypage, container, false)
+        return binding.root
     }
 
 }
