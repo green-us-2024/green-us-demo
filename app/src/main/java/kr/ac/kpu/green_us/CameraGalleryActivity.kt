e457f462120ebf6c627d56cc99aa396a0925c877
sejinzx <132969962+sejinzx@users.noreply.github.com>
2024-07-05 15:28:45 +0900

    리뷰 삭제 버튼

--- app/src/main/java/kr/ac/kpu/green_us/CameraGalleryActivity.kt
+++ app/src/main/java/kr/ac/kpu/green_us/CameraGalleryActivity.kt
@@ -16,7 +16,6 @@ class CameraGalleryActivity(private val context : AppCompatActivity) {
 
         dlg.requestWindowFeature(Window.FEATURE_NO_TITLE)   //타이틀바 제거
         dlg.setContentView(binding.root)     //다이얼로그에 사용할 xml 파일을 불러옴
-        dlg.setCancelable(false)    //다이얼로그의 바깥 화면을 눌렀을 때 다이얼로그가 닫히지 않도록 함
 
         // 카메라 버튼 동작
         binding.useCamera.setOnClickListener {
--- app/src/main/java/kr/ac/kpu/green_us/MyReviewActivity.kt
+++ app/src/main/java/kr/ac/kpu/green_us/MyReviewActivity.kt
@@ -1,12 +1,17 @@
 package kr.ac.kpu.green_us
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kr.ac.kpu.green_us.databinding.ActivityFaqBinding
 import kr.ac.kpu.green_us.databinding.ActivityMyReviewBinding
 
-class MyReviewActivity : AppCompatActivity() {
+class MyReviewActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityMyReviewBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +25,25 @@ class MyReviewActivity : AppCompatActivity() {
             intent.putExtra("key3","mypage")
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
             startActivity(intent)
+        }
+
+        // 삭제 버튼 클릭 시 삭제확인 창 띄우기
+        binding.delete.setOnClickListener(this)
+    }
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            binding.delete.id -> {
+                val dlg = DeleteCheckActivity(this)
+                dlg.setOnDeleteClickedListener { content ->
+                    if (content == 3) {
+
+                    }
+                    if (content == 4) {
+
+                    }
+                }
+                dlg.show()
+            }
         }
     }
 }
