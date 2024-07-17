e8cad0d1e03cf86fc5726da7a963dcfa3c0a60c0
yj_k <chloe_kang@naver.com>
2024-07-17 19:53:51 +0900

    Fix: 이메일, 비번 형식 수정

--- app/src/main/java/kr/ac/kpu/green_us/Join1Fragment.kt
+++ app/src/main/java/kr/ac/kpu/green_us/Join1Fragment.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -14,15 +15,21 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.core.graphics.toColorInt
 import androidx.core.view.isVisible
 import androidx.fragment.app.replace
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
+import kr.ac.kpu.green_us.common.RetrofitManager
+import kr.ac.kpu.green_us.common.api.RetrofitAPI
+import kr.ac.kpu.green_us.common.dto.Users
 import kr.ac.kpu.green_us.databinding.FragmentHomeBinding
 import kr.ac.kpu.green_us.databinding.FragmentJoin1Binding
 import org.w3c.dom.Text
+import java.util.regex.Pattern
+import javax.security.auth.callback.Callback
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,6 +41,8 @@ class Join1Fragment : Fragment() {
     private val binding get() = _binding!!
     private var email = ""
     private var pw = ""
+    val emailValidation = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -49,15 +58,30 @@ class Join1Fragment : Fragment() {
         // text input전 버튼 비활성화 -> 비밀번호까지 입력하면 활성화
         binding.btnNext.isEnabled = false
         binding.btnNext.alpha = 0.5f
-        binding.etPw.addTextChangedListener(object :TextWatcher{ //입력 감지 시기에 따른
+        // 이메일 형식 확인
+        binding.etEmail.addTextChangedListener(object :TextWatcher{ //입력 감지 시기에 따른
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {} //입력전
-            override fun afterTextChanged(p0: Editable?) {} // 입력후
+            override fun afterTextChanged(p0: Editable?) {
+            } // 입력후
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { //입력중
+                checkEmailValidation()
+            }
+        })
+        // 비밀번호 형식 확인
+        binding.etPw.addTextChangedListener(object :TextWatcher{
+            override fun afterTextChanged(p0: Editable?) {}
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 val et_email = binding.etEmail.text.toString().trim()
                 val et_pw = binding.etPw.text.toString().trim()
-                if (et_email.isNotEmpty()&&et_pw.isNotEmpty()){
+                if (et_pw.length >= 6 && et_email.isNotEmpty()){
+                    binding.tvPw.isVisible = false
+                    binding.etPw.setTextColor(resources.getColor(R.color.black))
                     binding.btnNext.isEnabled = true
                     binding.btnNext.alpha = 1f
+                }else{
+                    binding.etPw.setTextColor(resources.getColor(R.color.red))
+                    binding.tvPw.isVisible = true
                 }
             }
         })
@@ -77,8 +101,8 @@ class Join1Fragment : Fragment() {
         binding.btnNext.setOnClickListener {
             email = binding.etEmail.text.toString().trim()
             pw = binding.etPw.text.toString().trim()
-
             // db에 중복된 이메일이 있는지 확인. 중복된 이메일이 아니라면 번들에 데이터를 담아 다음 프래그먼트로 이동
+
             val bundle = Bundle()
             bundle.putString("email", email)
             bundle.putString("pw", pw)
@@ -89,6 +113,21 @@ class Join1Fragment : Fragment() {
             // 중복된 이메일이라면 이를 알리는 textview를 세팅
 //            binding.tvExist.isVisible = true
             }
+    }
+    fun checkEmailValidation():Boolean{
+        val email = binding.etEmail.text.toString().trim() //공백제거
+        val patternMatching = Pattern.matches(emailValidation, email) // 서로 패턴이 맞닝?
+        if (patternMatching) {
+            //이메일 형태가 정상일 경우
+            binding.etEmail.setTextColor(resources.getColor(R.color.black))
+            binding.tvEmail.isVisible = false
+            return true
+        } else {
+            binding.etEmail.setTextColor(resources.getColor(R.color.red))
+            binding.tvEmail.text = "올바른 이메일 형식으로 입력해 주세요"
+            binding.tvEmail.isVisible = true
+            return false
+        }
     }
 
 
--- app/src/main/java/kr/ac/kpu/green_us/common/api/RetrofitAPI.kt
+++ app/src/main/java/kr/ac/kpu/green_us/common/api/RetrofitAPI.kt
@@ -3,7 +3,10 @@ package kr.ac.kpu.green_us.common.api
 import kr.ac.kpu.green_us.common.dto.Users
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface RetrofitAPI {
     @POST("/users/new")
