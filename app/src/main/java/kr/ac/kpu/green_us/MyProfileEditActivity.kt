001244ceb0bd1037a599724ed044142df303b85b
sejinzx <132969962+sejinzx@users.noreply.github.com>
2024-08-01 00:57:08 +0900

    파이어베이스 클라우드에 이미지 올리기

--- app/src/main/java/kr/ac/kpu/green_us/MyProfileEditActivity.kt
+++ app/src/main/java/kr/ac/kpu/green_us/MyProfileEditActivity.kt
@@ -1,14 +1,14 @@
 package kr.ac.kpu.green_us
 
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
-import android.graphics.PorterDuff
-import android.graphics.PorterDuffXfermode
-import android.graphics.RectF
+import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
@@ -16,13 +16,18 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.google.firebase.Firebase
+import com.google.firebase.storage.storage
 import kr.ac.kpu.green_us.databinding.ActivityMyProfileEditBinding
-import java.io.File
+import java.io.ByteArrayOutputStream
+import java.text.SimpleDateFormat
+import java.util.*
 
 // 프로필 정보 수정 - 주소만 수정 가능
 class MyProfileEditActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityMyProfileEditBinding
     lateinit var bitmap: Bitmap
+    lateinit var uri: Uri
     var cameraOrGallery: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,6 +46,7 @@ class MyProfileEditActivity : AppCompatActivity(), View.OnClickListener {
 
         // 완료 버튼 클릭 시 activity_my_profile로 이동
         binding.complete.setOnClickListener {
+            imageUpload(uri)
             val intent = Intent(this, MyProfileActivity::class.java)
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
             startActivity(intent)
@@ -104,18 +110,48 @@ class MyProfileEditActivity : AppCompatActivity(), View.OnClickListener {
         if (it.resultCode == RESULT_OK && it.data != null) {
             if(cameraOrGallery == 1){
                 val extras = it.data!!.extras
-                bitmap = extras?.get("data") as Bitmap
+                bitmap = extras!!.get("data") as Bitmap
+                uri = getImageUri(this@MyProfileEditActivity, bitmap)
                 binding.userImg.setImageBitmap(bitmap)
                 binding.userImg.clipToOutline = true
+
             }
             if(cameraOrGallery == 2){
-                val uri = it.data!!.data
+                uri = it.data?.data!!
                 Glide.with(this)
                     .load(uri)
                     .apply(RequestOptions().circleCrop())
                     .into(binding.userImg)
                 binding.userImg.clipToOutline = true
             }
+        }
+    }
+
+    fun getImageUri(inContext: Context?, inImage: Bitmap?): Uri {
+        val bytes = ByteArrayOutputStream()
+        if (inImage != null) {
+            inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
+        }
+        val path = MediaStore.Images.Media.insertImage(inContext?.getContentResolver(), inImage, "Title" + " - " + Calendar.getInstance().getTime(), null)
+        return Uri.parse(path)
+    }
+
+    private fun imageUpload(uri: Uri) {
+        // storage 인스턴스 생성
+        val storage = Firebase.storage
+        // storage 참조
+        val storageRef = storage.getReference("image")
+        // storage에 저장할 파일명 선언
+        val fileName = SimpleDateFormat("yyyyMMddHHmmss").format(Date())
+        val mountainsRef = storageRef.child("${fileName}.png")
+
+        val uploadTask = mountainsRef.putFile(uri)
+        uploadTask.addOnSuccessListener { taskSnapshot ->
+            // 파일 업로드 성공
+            Toast.makeText(this, "사진 업로드 성공", Toast.LENGTH_SHORT).show();
+        }.addOnFailureListener {
+            // 파일 업로드 실패
+            Toast.makeText(this, "사진 업로드 실패", Toast.LENGTH_SHORT).show();
         }
     }
 
