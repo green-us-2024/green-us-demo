3d1b4592f92ecfadc2fa86147d43b7bcc3551850
leedanbii <eksqlgodqhr@naver.com>
2024-08-07 18:45:50 +0900

    update: Participate and MyGreening

--- app/src/main/java/kr/ac/kpu/green_us/common/api/RetrofitAPI.kt
+++ app/src/main/java/kr/ac/kpu/green_us/common/api/RetrofitAPI.kt
@@ -2,11 +2,13 @@ package kr.ac.kpu.green_us.common.api
 
 import kr.ac.kpu.green_us.common.dto.Greening
 import kr.ac.kpu.green_us.common.dto.Notice
+import kr.ac.kpu.green_us.common.dto.Participate
 import kr.ac.kpu.green_us.common.dto.Users
 import kr.ac.kpu.green_us.common.dto.User
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.POST
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -44,6 +46,12 @@ interface RetrofitAPI {
 
     @GET("/greening/byUserSeq/{userSeq}")
     fun getGreeningByUserSeq(@Path("userSeq") userSeq: Int): Call<List<Greening>>
+
+    @POST("/participate/new")
+    fun registerParticipate(@Body participate: Participate): Call<Participate>
+
+    @GET("/participate/GreeningByUserSeq/{userSeq}")
+    fun findGreeningByUserSeq(@Path("userSeq") userSeq: Int): Call<List<Greening>>
 
     @GET("/notice/list")
     fun getNotices():Call<List<Notice>>
