23348b196fc31e40c2cb2e45950524d659eab6d5
yj_k <chloe_kang@naver.com>
2024-08-14 11:55:11 +0900

    Fix : 프로필 이름 불러오기
    현재 로그인한 사용자에 따른 프로필 이름 불러오기

--- app/src/main/java/kr/ac/kpu/green_us/MypageFragment.kt
+++ app/src/main/java/kr/ac/kpu/green_us/MypageFragment.kt
@@ -11,12 +11,14 @@ import com.bumptech.glide.Glide
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
-import com.google.firebase.firestore.auth.User
 import com.google.firebase.storage.storage
 import kr.ac.kpu.green_us.common.RetrofitManager
 import kr.ac.kpu.green_us.common.api.RetrofitAPI
+import kr.ac.kpu.green_us.common.dto.User
 import kr.ac.kpu.green_us.databinding.FragmentMypageBinding
-import okhttp3.Callback
+import retrofit2.Callback
+import okhttp3.Response
+import retrofit2.Call
 
 // 마이페이지 - 포인트, 개설하기, 내리뷰, 프로필관리, 공지사항, FAQ, 고객센터 화면으로 이동 가능
 class MypageFragment : Fragment() {
@@ -42,10 +44,10 @@ class MypageFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // 프로필이미지
-        binding.userImg.clipToOutline = true
-        uploadImgToProfile(uid)
-        settingUserName(userEmail)
+        // 프로필
+        binding.userImg.clipToOutline = true //이미지 둥글게
+        uploadImgToProfile(uid) // 현재 로그인한 회원에 맞는 이미지 불러오기
+        settingUserName(userEmail) // 현재 로그인한 회원에 맞는 이름 불러오기
 
         // 개설하기
         binding.goToGreenOpen.setOnClickListener {
@@ -103,14 +105,25 @@ class MypageFragment : Fragment() {
         val storage = Firebase.storage
         val storageRef = storage.getReference("profileImgs/${uid}")
         storageRef.downloadUrl.addOnSuccessListener {
-            Log.d("profileImg",it.toString())
             Glide.with(this).load(it).into(binding.userImg)
         }.addOnFailureListener {
             Log.d("profileImg","사진 불러오기 실패")
         }
     }
     private fun settingUserName(userEmail:String){
+        val retrofitAPI = RetrofitManager.retrofit.create(RetrofitAPI::class.java)
+        retrofitAPI.getUserbyEmail(userEmail).enqueue(object :Callback<User>{
+            override fun onResponse(call: Call<User>, response: retrofit2.Response<User>) {
+                if(response.isSuccessful){
+                    val userName = response.body()?.userName.toString()
+                    binding.name.text = userName
+                }
+            }
+            override fun onFailure(call: Call<User>, t: Throwable) {
+                Log.d("settingUserName-retrofitApi","불러오기 실패")
+            }
 
+        })
     }
 
 }
