f4a5693c9ab8fdcbf0dbe08fb1bb5b5ed0a8dd69
leedanbii <eksqlgodqhr@naver.com>
2024-09-10 14:55:48 +0900

    PrizeUpdateScheduler.kt

--- src/main/kotlin/com/example/greening/repository/PrizeRepository.kt
+++ src/main/kotlin/com/example/greening/repository/PrizeRepository.kt
@@ -5,12 +5,14 @@ import com.example.greening.domain.item.Prize
 import jakarta.persistence.EntityManager
 import jakarta.persistence.PersistenceContext
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
 interface PrizeRepository : JpaRepository<Prize, Int> {
     fun findByUser_UserSeq(userSeq: Int): List<Prize>
 
+    @Query("select p from Prize p where p.participate.pSeq = :pSeq")
     fun findByParticipate_PSeq(pSeq: Int): Prize?
 
 }
